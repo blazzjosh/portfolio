873425156b3f575f70d977e6508cb22371e1616a
blazzjosh <blazzjosh@live.com>
2022-05-12 12:34:41 +0100

    include mailTo:

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -132,9 +132,10 @@
 							d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
 						/>
 					</svg>
+					
 				</button>
 
-				<button
+				<a href="mailto:cradlyjosh@gmail.com"
 					type="button"
 					class="flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
 				>
@@ -154,7 +155,7 @@
 							d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
 						/>
 					</svg>
-				</button>
+				</a>
 			</div>
 		</div>
 		<nav class="hidden lg:py-2 lg:flex lg:space-x-8 " aria-label="Global">
